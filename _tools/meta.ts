024e5e26f14a468a63a1821aeaa4d3eb290b2659
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-03-17 09:45:09 +0900

    chore(_tools): add compiler options

--- _tools/meta.ts
+++ _tools/meta.ts
@@ -3,6 +3,9 @@ import { BuildOptions } from "https://deno.land/x/dnt@0.33.1/mod.ts";
 export const makeOptions = (version: string): BuildOptions => ({
   test: false,
   shims: {},
+  compilerOptions: {
+    lib: ["dom", "esnext", "dom.iterable"],
+  },
   typeCheck: true,
   entryPoints: ["./mod.ts"],
   outDir: "./npm",
