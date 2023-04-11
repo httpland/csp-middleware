31727d0598c41a1bff71fe2623fed01139f9e554
Tomoki Miyauchi <development.operation.6.6.6@gmail.com>
2023-04-11 11:27:22 +0900

    feat(mod): export related types

--- mod.ts
+++ mod.ts
@@ -3,3 +3,24 @@
 
 export { csp, type Options } from "./middleware.ts";
 export { type Handler, type Middleware } from "./deps.ts";
+export type {
+  AncestorSource,
+  AncestorSourceList,
+  Base64Value,
+  CamelCasingCSPDirectives,
+  CSPDirectives,
+  DocumentDirectives,
+  FetchDirectives,
+  HashAlgorithm,
+  HashSource,
+  HostSource,
+  KeywordSource,
+  NavigationDirectives,
+  NonceSource,
+  OtherDirectives,
+  ReportingDirectives,
+  SchemeSource,
+  SourceExpression,
+  SourceList,
+  UriReference,
+} from "./types.ts";
